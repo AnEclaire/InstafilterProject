7a14fe559b35f694284bd7aa07fbc4b1356a534e
Emma Gutierrez <eclaire.tv@gmail.com>
2023-03-13 19:05:06 -0700

    Initial Commit

---
+++ InstafilterProject/InstafilterProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterProjectApp.swift
+//  InstafilterProject
+//
+//  Created by Emma Gutierrez on 3/13/23.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
