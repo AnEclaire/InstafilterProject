3cb8c36ef954541ade404b7190289fa1f2c93820
Emma Gutierrez <eclaire.tv@gmail.com>
2023-03-14 23:41:27 -0700

    Created initial ImagePicker custom view class.

--- InstafilterProject/ContentView.swift
+++ InstafilterProject/ContentView.swift
@@ -5,17 +5,27 @@
 //  Created by Emma Gutierrez on 3/13/23.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
+    @State private var image: Image?
+    @State private var showingImagePicker = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            image?
+                .resizable()
+                .scaledToFit()
+            
+            Button("Select Image") {
+                showingImagePicker = true
+            }
         }
-        .padding()
+        .sheet(isPresented: $showingImagePicker) {
+            ImagePicker()
+        }
     }
 }
 
---
+++ InstafilterProject/ImagePicker.swift
@@ -0,0 +1,23 @@
+//
+//  ImagePicker.swift
+//  InstafilterProject
+//
+//  Created by Emma Gutierrez on 3/14/23.
+//
+
+import PhotosUI
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        var config = PHPickerConfiguration()
+        config.filter = .images
+        
+        let picker = PHPickerViewController(configuration: config)
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {
+    
+    }
+}
