8e81e7b5bf08bb6cbd413b7ace56f6d3f122adf2
Emma Gutierrez <eclaire.tv@gmail.com>
2023-03-15 23:08:05 -0700

    ImagePicker.swift updated to uses a coordinator to help load the image properly in main.

--- InstafilterProject/ImagePicker.swift
+++ InstafilterProject/ImagePicker.swift
@@ -8,16 +8,45 @@
 import PhotosUI
 import SwiftUI
 
+// ImagePicker struct that knows how to make a PHPicker View Controller
+// We've told ImagePicker that it should handle communication with said view controller with our coordinator class.
 struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    
+    class Coordinator: NSObject, PHPickerViewControllerDelegate {
+        var parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            picker.dismiss(animated: true)
+            
+            guard let provider = results.first?.itemProvider else { return }
+            
+            if provider.canLoadObject(ofClass: UIImage.self) {
+                provider.loadObject(ofClass: UIImage.self) { image, _ in
+                    self.parent.image = image as? UIImage
+                }
+            }
+        }
+    }
+    
     func makeUIViewController(context: Context) -> PHPickerViewController {
         var config = PHPickerConfiguration()
         config.filter = .images
         
         let picker = PHPickerViewController(configuration: config)
+        picker.delegate = context.coordinator // tells swift to use this coordinater class as a delegate for our Image Picker View Controller
         return picker
     }
     
     func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {
     
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
     }
 }
