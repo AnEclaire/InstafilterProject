53a00b25068d7be18180f114ff22b4d96a8bcbc7
Emma Gutierrez <eclaire.tv@gmail.com>
2023-03-15 23:26:23 -0700

    Created image saver class for use in main project.

--- InstafilterProject/ContentView.swift
+++ InstafilterProject/ContentView.swift
@@ -5,34 +5,12 @@
 //  Created by Emma Gutierrez on 3/13/23.
 //
 
-import CoreImage
-import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
-    @State private var image: Image?
-    @State private var inputImage: UIImage?
-    @State private var showingImagePicker = false
     
     var body: some View {
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
-            
-            Button("Select Image") {
-                showingImagePicker = true
-            }
-        }
-        .sheet(isPresented: $showingImagePicker) {
-            ImagePicker(image: $inputImage)
-        }
-        .onChange(of: inputImage) { _ in loadImage() }
-    }
-    
-    func loadImage() {
-        guard let inputImage = inputImage else { return }
-        image = Image(uiImage: inputImage)
+        Text("Hello world!")
     }
 }
 
